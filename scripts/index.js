bc00eeab138564b212509e05706afc070c091739
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2022-04-17 02:26:18 +0200

    Refactoring: Use backtick to define template literals to make working with strings easier

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 document.getElementById("copyrightyear").innerHTML = String(new Date().getFullYear());
 
-async function postData() {
+function fetchData() {
     const DATA_URL = "https://rtbf.ir/data/data.json"
 
     return fetch(DATA_URL)
@@ -12,41 +12,76 @@ async function postData() {
 }
 
 function appendCard(website) {
-    $(".myitems").append(
-        '<div class="float-right"><div class="card"><div class="card-body"><h5 class="card-title"><a target="_blank" class="text-dark" rel="nofollow" title="سایت ' +
-        website.name +
-        '" href="' +
-        website.website +
-        '">' +
-        website.name +
-        '</a>&nbsp;<a href="https://github.com/amirshnll/rtbf.ir/issues/new/choose" style="font-size:12px;" class="text-warning" title="اطلاعات این بخش اشتباه است قصد اصلاح دارم"><small><span class="fas fa-1x fa-bolt"></span> تصحیح<small></a></h5><p class="card-text">' +
-        website.info +
-        '</p><div class="gap-card"></div><div><p class="card-opt"><a class="btn text-light ' +
-        website.keytype +
-        '">' +
-        website.difficulty +
-        "</a></p></div></div></div></div>"
-    );
+    const websiteCard = `<div class="float-right">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <a target="_blank" class="text-dark" rel="nofollow" title="سایت ${website.name}" href="${website.website}"/>
+                            ${website.name}
+                        </a>
+                        <a href="https://github.com/amirshnll/rtbf.ir/issues/new/choose"
+                            style="font-size:12px;"
+                            class="text-warning" title="اطلاعات این بخش اشتباه است قصد اصلاح دارم">
+                                <small>
+                                    <span class="fas fa-1x fa-bolt"></span>
+                                 تصحیح<small>
+                         </a>
+                    </h5>
+
+                    <p class="card-text"><small>${website.info}</small></p>
+
+                    <div class="gap-card"></div>
+
+                    <div>
+                        <p class="card-opt">
+                            <a class="btn text-light ${website.keytype}">${website.difficulty}</a>
+                        </p>
+                     </div>
+                </div>
+            </div>
+         </div>`
+
+
+    document.getElementsByClassName("myitems")[0]
+        .insertAdjacentHTML('beforeend', websiteCard)
 }
 
-function appendCardWithRemoveButton(website) {
-    $(".myitems").append(
-        '<div class="float-right"><div class="card"><div class="card-body"><h5 class="card-title"><a target="_blank" class="text-dark" rel="nofollow" title="سایت ' +
-        website.name +
-        '" href="' +
-        website.website +
-        '">' +
-        website.name +
-        '</a>&nbsp;<a href="https://github.com/amirshnll/rtbf.ir/issues/new/choose" style="font-size:12px;" class="text-warning" title="اطلاعات این بخش اشتباه است قصد اصلاح دارم"><small><span class="fas fa-1x fa-bolt"></span> تصحیح<small></a></h5><p class="card-text">' +
-        website.info +
-        '</p><div class="gap-card"></div><div><p class="card-opt"><a class="btn text-light ' +
-        website.keytype +
-        '">' +
-        website.difficulty +
-        '</a>&nbsp;<a target="_blank" rel="nofollow" href="' +
-        website.deleteurl +
-        '" class="btn label-remove"><span class="fa fa-1x fa-trash"></span> &nbsp;حذف حساب کاربری</a></p></div></div></div></div>'
-    );
+function AppendCardWithDeleteAccountButton(website) {
+    const websiteCard = `<div class="float-right test-mohammad">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <a target="_blank" class="text-dark" rel="nofollow" title="سایت ${website.name}" href="${website.website}">
+                            ${website.name}
+                        </a>
+                        <a href="https://github.com/amirshnll/rtbf.ir/issues/new/choose"
+                            style="font-size:12px;"
+                            class="text-warning" title="اطلاعات این بخش اشتباه است قصد اصلاح دارم">
+                                <small>
+                                    <span class="fas fa-1x fa-bolt"></span>
+                                تصحیح<small>
+                        </a>
+                    </h5>
+
+                    <p class="card-text"><small>${website.info}</small></p>
+
+                    <div class="gap-card"></div>
+
+                    <div>
+                        <p class="card-opt">
+                            <a class="btn text-light ${website.keytype}">${website.difficulty}</a>
+                            <a target="_blank" rel="nofollow" href="${website.deleteurl}" class="btn label-remove">
+                                <span class="fa fa-1x fa-trash"></span>
+                                    &nbsp;حذف حساب کاربری
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+          </div>`
+
+    document.getElementsByClassName("myitems")[0]
+        .insertAdjacentHTML('beforeend', websiteCard)
 }
 
 function addNewWebsiteButton() {
@@ -64,14 +99,14 @@ function addNewWebsiteButton() {
 }
 
 async function appendToDOM() {
-    const websites = await postData()
-    const sortedData =  websites.sort((a, b) => b.name.localeCompare(a.name))
-    const websitesCount = sortedData.length - 1
+    const websites = await fetchData()
+    const sortedAlphabetically =  websites.sort((a, b) => b.name.localeCompare(a.name))
+    const websitesCount = sortedAlphabetically.length - 1
 
     for (let i = websitesCount; i >= 0; i--) {
-        sortedData[i].deleteurl === "#"
-            ? appendCard(sortedData[i])
-            : appendCardWithRemoveButton(sortedData[i]);
+        sortedAlphabetically[i].deleteurl === "#"
+            ? appendCard(sortedAlphabetically[i])
+            : AppendCardWithDeleteAccountButton(sortedAlphabetically[i]);
     }
 
     addNewWebsiteButton()
