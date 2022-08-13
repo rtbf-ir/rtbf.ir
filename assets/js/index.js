2c810ff06be15f9566c9009b087b6bf30c29ca91
Amir Shokri <amirsh.nll@gmail.com>
2022-08-13 11:25:29 +0430

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -151,6 +151,7 @@ async function changeData(category, priority, status, service_name) {
   }
 
   if (service_name != "") {
+    console.log(service_name);
     for (let index = 0; index < items.length; index++) {
       const element = items[index];
       if (element["name"].search(service_name) != -1) {
