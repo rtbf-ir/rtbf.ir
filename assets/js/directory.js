e01e06f24ac12905ae7ef502889f74a720c5d40f
Amir Shokri <amirsh.nll@gmail.com>
2024-02-06 18:37:22 +0330

    copyright number #243
    convert to persian number

--- assets/js/directory.js
+++ assets/js/directory.js
@@ -1,4 +1,23 @@
-document.getElementById("copyrightyear").innerHTML = String(
+function toPersianNumber(number) {
+  const numbersMap = {
+    0: "۰",
+    1: "۱",
+    2: "۲",
+    3: "۳",
+    4: "۴",
+    5: "۵",
+    6: "۶",
+    7: "۷",
+    8: "۸",
+    9: "۹",
+  };
+  return String(number)
+    .split("")
+    .map((digit) => numbersMap[digit] || digit)
+    .join("");
+}
+
+document.getElementById("copyrightyear").innerHTML = toPersianNumber(
   new Date().getFullYear()
 );
 
